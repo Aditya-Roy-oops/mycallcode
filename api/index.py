a44852ba5606967018e7070824dc0af55d1cd48e
Aditya Sharma <asrpcp11@gmail.com>
2026-01-02 21:24:58 +0530

    Add '/stealth' route to Flask application

--- api/index.py
+++ api/index.py
@@ -6,5 +6,10 @@ app = Flask(__name__, template_folder='../templates')
 def index():
     return render_template('index.html')
 
+@app.route('/stealth')
+def secure():
+    return render_template('stealth.html')
+
 if __name__ == '__main__':
     app.run()
+
