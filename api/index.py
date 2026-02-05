59c3f5ce2aa85086701400ba52f9590311d91534
Aditya Sharma <asrpcp11@gmail.com>
2026-02-06 00:12:08 +0530

    Simplify index route and remove debug mode
    Removed security context comments and debug mode.

--- api/index.py
+++ api/index.py
@@ -4,9 +4,7 @@ app = Flask(__name__, template_folder='../templates')
 
 @app.route('/')
 def index():
-    # Headers to ensure security context for Camera/Mic
-    response = render_template('index.html')
-    return response
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
